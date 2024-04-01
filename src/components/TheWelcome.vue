50af79fbef50c9916bc6b2e30390a57fc49455a7
nurazima <nurazima270@gmail.com>
2024-04-01 22:00:44 +0700

    first-commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,81 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Introduction </template>
+
+    
+    <a href="https://vuejs.org/" target="_blank" rel="noopener"></a>
+    perkenalkan nama saya NUR AZIMA ,bisa dipanggil zima saya seorang mahasiswa dari Universitas Islam Riau 
+    fakul teknik prodi teknik informatika semester 4
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>pendidikan</template>
+
+    SMA : SMKN1 RIMBA MELINTANG<br>
+    SMP : Mts.NURUL BILAD<br>
+    SD  : SD NEGERI 008<br>
+
+    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener"></a>
+    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener"></a>
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener"></a>
+    <a href="https://www.cypress.io/" target="_blank" rel="noopener"></a>
+    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
+    </a>
+
+    <br />
+
+     <code></code>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Alamat</template>
+
+    JL.Kharuddin Nasution gang haji sulaiman ,Pekanbaru,Riau
+    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener"></a>
+    <a href="https://router.vuejs.org/" target="_blank" rel="noopener"></a>
+    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener"></a>
+    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener"></a>
+    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener"></a>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>hobi</template>
+
+    memasak
+    <a href="https://chat.vuejs.org" target="_blank" rel="noopener"></a>
+    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"></a>
+    <a href="https://news.vuejs.org" target="_blank" rel="noopener"></a>
+    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"></a>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>media sosial</template>
+
+@azimaar.
+    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener"></a>
+  </WelcomeItem>
+</template>
